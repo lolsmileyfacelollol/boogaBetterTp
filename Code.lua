7a137c54087694b709539acebe7bfba552233bd1
lolsmileyfacelollol <115596442+lolsmileyfacelollol@users.noreply.github.com>
2022-10-13 11:42:05 -0400

    Update Code.lua

--- Code.lua
+++ Code.lua
@@ -48,7 +48,7 @@ FLYING = false
 iyflyspeed = 0.25
 vehicleflyspeed = 0.25
 local function TpArea(part)
-    for i = 0,1,0.001 do
+    for i = 0,1,0.005 do
         task.wait()
         Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Players.LocalPlayer.Character.HumanoidRootPart.CFrame:Lerp(part.CFrame+Vector3.new(0,part.Size.Y,0), i)
         if i == 1 then
